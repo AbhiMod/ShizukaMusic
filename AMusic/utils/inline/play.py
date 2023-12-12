0807ac478e7d8aa7bf01ca285d1c4ed5a7cc8d3e
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-12-12 14:47:05 +0530

    Update play.py

--- AMusic/utils/inline/play.py
+++ AMusic/utils/inline/play.py
@@ -61,8 +61,8 @@ def stream_markup_timer(_, chat_id, played, dur):
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text="ᴄʜᴀᴛ ɢʀᴏᴜᴘ", url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text="ᴏᴡɴᴇʀ", url=f"https://t.me/sam_loveall"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
@@ -72,8 +72,8 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text="ᴄʜᴀᴛ ɢʀᴏᴜᴘ", url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text="ᴏᴡɴᴇʀ", url=f"https://t.me/sam_loveall"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
