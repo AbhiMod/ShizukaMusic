cfcfc8589bc38ad20fd38bfa288393b1ab38c63a
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-12-12 15:49:48 +0530

    Update start.py

--- AMusic/plugins/bot/start.py
+++ AMusic/plugins/bot/start.py
@@ -1,15 +1,14 @@
 import time
 import random
-
 from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from youtubesearchpython.__future__ import VideosSearch
 
 import config
-from config import START_IMG_URL
 from AMusic import app
 from AMusic.misc import _boot_
+from AMusic.utils.database import get_served_chats, get_served_users, get_sudoers
 from AMusic.plugins.sudo.sudoers import sudoers_list
 from AMusic.utils.database import (
     add_served_chat,
@@ -19,12 +18,12 @@ from AMusic.utils.database import (
     is_banned_user,
     is_on_off,
 )
+from AMusic.utils import bot_sys_stats
 from AMusic.utils.decorators.language import LanguageStart
 from AMusic.utils.formatters import get_readable_time
 from AMusic.utils.inline import help_pannel, private_panel, start_panel
-from config import BANNED_USERS
+from config import BANNED_USERS, START_IMG_URL
 from strings import get_string
-
 
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
@@ -86,9 +85,12 @@ async def start_pm(client, message: Message, _):
                 )
     else:
         out = private_panel(_)
+        served_chats = len(await get_served_chats())
+        served_users = len(await get_served_users())
+        UP, CPU, RAM, DISK = await bot_sys_stats()
         await message.reply_photo(
             photo=random.choice(START_IMG_URL),
-            caption=_["start_2"].format(message.from_user.mention, app.mention),
+            caption=_["start_2"].format(message.from_user.mention, app.mention, UP, DISK, CPU, RAM,served_users,served_chats),
             reply_markup=InlineKeyboardMarkup(out),
         )
         if await is_on_off(2):
@@ -102,6 +104,9 @@ async def start_pm(client, message: Message, _):
 @LanguageStart
 async def start_gp(client, message: Message, _):
     out = start_panel(_)
+    served_chats = len(await get_served_chats())
+    served_users = len(await get_served_users())
+    UP, CPU, RAM, DISK = await bot_sys_stats()
     uptime = int(time.time() - _boot_)
     await message.reply_photo(
         photo=random.choice(START_IMG_URL),
@@ -138,6 +143,9 @@ async def welcome(client, message: Message):
                     return await app.leave_chat(message.chat.id)
 
                 out = start_panel(_)
+                served_chats = len(await get_served_chats())
+                served_users = len(await get_served_users())
+                UP, CPU, RAM, DISK = await bot_sys_stats()
                 await message.reply_photo(
                     photo=random.choice(START_IMG_URL),
                     caption=_["start_3"].format(
